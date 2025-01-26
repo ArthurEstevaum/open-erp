be9f0c0cb4f9811a09c1852e0f51e84e2768980b
ArthurEstevaum <arthurestevao631@gmail.com>
2025-01-26 16:53:14 -0300

    Bootstrap project

---
+++ backend/src/test/java/com/estevaum/open_erp/OpenErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estevaum.open_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
