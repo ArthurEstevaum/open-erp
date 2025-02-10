c3839a19bcd9c4818831f4c8a8c96cdb3b201b9a
ArthurEstevaum <arthurestevao631@gmail.com>
2025-02-10 16:58:01 -0300

    Format service files

--- frontend/src/services/addPermissionService.ts
+++ frontend/src/services/addPermissionService.ts
@@ -1,11 +1,16 @@
-export default async function addPermissionService(token: string, username: string, permissionList: string[]) {
-    const fetchOptions = {
-        method: "POST",
-        body: JSON.stringify({ permissionList, username }),
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    }
-    return await fetch("http://localhost:8080/users/permissions", fetchOptions)
+export default async function addPermissionService(
+  token: string,
+  username: string,
+  permissionList: string[],
+) {
+  const fetchOptions = {
+    method: "POST",
+    body: JSON.stringify({ permissionList, username }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await fetch("http://localhost:8080/users/permissions", fetchOptions);
 }
+
